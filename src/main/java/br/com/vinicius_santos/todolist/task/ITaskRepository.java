99dd05a8522e86bb22019c4f3d6295e6e8fe3c75
Vinícius Santos Oliveira <vinicius.oliveira@institutojef.org.br>
2024-10-06 19:14:18 -0300

    feat: create task repository

---
+++ src/main/java/br/com/vinicius_santos/todolist/task/ITaskRepository.java
@@ -0,0 +1,7 @@
+package br.com.vinicius_santos.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {}
