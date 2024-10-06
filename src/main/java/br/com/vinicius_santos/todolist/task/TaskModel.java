c1055ecdafd679d25613a547eb2a0727e33f61c9
Vinícius Santos Oliveira <vinicius.oliveira@institutojef.org.br>
2024-10-06 18:58:38 -0300

    task: create task model

---
+++ src/main/java/br/com/vinicius_santos/todolist/task/TaskModel.java
@@ -0,0 +1,35 @@
+package br.com.vinicius_santos.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tbl_tasks")
+public class TaskModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private UUID userId;
+
+}
